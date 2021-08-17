f161ac86301fb65306542faa2554325685275525
Aniket Kuwar <aniket.kuwar@vonage.com>
2021-08-16 23:13:17 -0400

    initial changes for sqlite memory leak

--- modules/db_sqlite/dbase.c
+++ modules/db_sqlite/dbase.c
@@ -265,10 +265,7 @@ int db_sqlite_fetch_result(const db_con_t* _h, db_res_t** _r, const int nrows)
 		}
 	} else {
 		/* free old rows */
-		if(RES_ROWS(*_r)!=0)
-			db_free_rows(*_r);
-		RES_ROWS(*_r) = 0;
-		RES_ROW_N(*_r) = 0;
+		db_sqlite_free_result_rows(*_r);
 	}
 
 	/* determine the number of rows remaining to be processed */
@@ -822,6 +819,54 @@ int db_sqlite_free_result(db_con_t* _h, db_res_t* _r)
 	_r = NULL;
 
 	return 0;
+}
+
+/**
+ * Release a result set from memory.
+ * \param _r result set whose rows and values should be freed
+ * \return void
+ */
+void db_sqlite_free_result_rows(db_res_t* _r)
+{
+	int i, j;
+	db_val_t* val;
+
+	if (!_r) {
+		LM_DBG("nothing to free!\n");
+		return;
+	}
+
+	if (RES_ROWS(*_r)) {
+		for(i=0; i < RES_ROW_N(*_r); i++) {
+			for (j=0; j < RES_COL_N(*_r); j++) {
+				val = &((*_r)->rows[i].values[j]);
+				if (VAL_NULL(val) || !VAL_FREE(val))
+					continue;
+
+				switch (VAL_TYPE(val)) {
+					case DB_STRING:
+					case DB_STR:
+						pkg_free(VAL_STR(val).s);
+						VAL_STR(val).s = 0;
+						break;
+					case DB_BLOB:
+						pkg_free(VAL_BLOB(val).s);
+						VAL_BLOB(val).s = 0;
+						break;
+					default:
+						break;
+						}
+
+					}
+				}
+				/* free all the columns; they are all allocated at once */
+				pkg_free( (*_r)->rows[0].values);
+				/* free the rows */
+				pkg_free( (*_r)->rows);
+				(*_r)->rows = NULL;
+			}
+
+	RES_ROW_N(*_r) = 0;
 }
 
 /**
