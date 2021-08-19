3147b50a9d5b1e2070f1acad8975197682ebac21
Aniket Kuwar <aniket.kuwar@vonage.com>
2021-08-19 12:01:19 -0400

    clusterer: fix possible crash when sending a bin packet

--- modules/clusterer/sync.c
+++ modules/clusterer/sync.c
@@ -60,7 +60,7 @@ static int get_sync_source(cluster_info_t *cluster, str *capability,
 	struct remote_cap *cap;
 
 	for (node = cluster->node_list; node; node = node->next) {
-		if (get_next_hop(node) <= 0)
+		if (get_next_hop(node) == 0)
 			continue;
 
 		if (!match_node(cluster->current_node, node, match_cond))
@@ -292,15 +292,13 @@ void handle_sync_request(bin_packet_t *packet, cluster_info_t *cluster,
 {
 	str cap_name;
 	struct remote_cap *cap;
-	int nhop;
 
 	bin_pop_str(packet, &cap_name);
 
 	LM_DBG("Received sync request for capability: %.*s from: %d\n", cap_name.len,
 		cap_name.s, source->node_id);
 
-	nhop = get_next_hop(source);
-	if (nhop > 0) {
+	if (get_next_hop(source)) {
 		send_sync_repl(cluster, source->node_id, &cap_name);
 	} else {
 		lock_get(source->lock);
